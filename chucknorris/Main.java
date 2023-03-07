aba0763ceea19c62a43eb028f48ec4fe0cecf811
Jon Sherwin <scapegrace@gmail.com>
2023-03-07 14:44:40 -0800

    test commit

--- chucknorris/Main.java
+++ chucknorris/Main.java
@@ -10,6 +10,7 @@ public class Main {
         for (int i = 0; i < input.length(); i++) {
             letters[i] = input.charAt(i);
             System.out.print(letters[i] + " ");
+            //test commit
         }
 
     }
