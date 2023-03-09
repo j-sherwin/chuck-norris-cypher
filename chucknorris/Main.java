af22ad70b5eea6cff6f57c574063121d1f63adee
underdog378 <scapegrace@gmail.com>
2023-03-08 21:52:56 -0800

    Stage 3/5 Update 2

--- chucknorris/Main.java
+++ chucknorris/Main.java
@@ -6,25 +6,27 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Input string:");
         String input = scanner.nextLine();
-        char[] letters = new char[input.length()];
-        System.out.println("The result:");
         String lettersInBinary = "";
+        char[] letters = new char[input.length()];
+
+        System.out.println("The result:");
+
         for (int i = 0; i < input.length(); i++) {
             letters[i] = input.charAt(i);
-            String binaryLetters = Integer.toBinaryString(letters[i]);
-            int num = Integer.parseInt(binaryLetters);
+            String s = Integer.toBinaryString(letters[i]);
+            int num = Integer.parseInt(s);
             lettersInBinary = lettersInBinary.concat(String.format("%07d", num));
         }
+
         for (int j = 0; j <lettersInBinary.length(); j++) {
             int count = 1;
-            while (j + 1 < lettersInBinary.length() && lettersInBinary.charAt(j) == lettersInBinary.charAt(j +1)) {
+            while (j + 1 < lettersInBinary.length() && lettersInBinary.charAt(j) == lettersInBinary.charAt(j + 1)) {
                 j++;
                 count++;
             }
             if (lettersInBinary.charAt(j) == '1') {
                 System.out.print("0 " + "0".repeat(count) + " ");
-            }
-            if (lettersInBinary.charAt(j) == '0') {
+            } else {
                 System.out.print("00 " + "0".repeat(count) + " ");
             }
         }
