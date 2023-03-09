80cf82cc59aa1741a9c43a9f5b03994c9e9b1808
underdog378 <scapegrace@gmail.com>
2023-03-08 21:28:14 -0800

    Stage 3/5

--- chucknorris/Main.java
+++ chucknorris/Main.java
@@ -8,12 +8,29 @@ public class Main {
         String input = scanner.nextLine();
         char[] letters = new char[input.length()];
         System.out.println("The result:");
+        String lettersInBinary = "";
         for (int i = 0; i < input.length(); i++) {
             letters[i] = input.charAt(i);
-            String lettersInBinary = Integer.toBinaryString(letters[i]);
-            int num = Integer.parseInt(lettersInBinary);
-            System.out.println(letters[i] + " = " + String.format("%07d", num));
+            String binaryLetters = Integer.toBinaryString(letters[i]);
+            int num = Integer.parseInt(binaryLetters);
+            lettersInBinary = lettersInBinary.concat(String.format("%07d", num));
         }
-
+        for (int j = 0; j <lettersInBinary.length(); j++) {
+            int count = 1;
+            while (j + 1 < lettersInBinary.length() && lettersInBinary.charAt(j) == lettersInBinary.charAt(j +1)) {
+                j++;
+                count++;
+            }
+            if (lettersInBinary.charAt(j) == '1') {
+                System.out.print("0 ");
+                System.out.print("0".repeat(count));
+                System.out.print(" ");
+            }
+            if (lettersInBinary.charAt(j) == '0') {
+                System.out.print("00 ");
+                System.out.print("0".repeat(count));
+                System.out.print(" ");
+            }
+        }
     }
 }
