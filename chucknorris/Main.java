96d6305d943b119c939946332cf3781a40616d36
underdog378 <scapegrace@gmail.com>
2023-03-14 13:41:35 -0700

    Stage 4/5 Update

--- chucknorris/Main.java
+++ chucknorris/Main.java
@@ -1,11 +1,34 @@
 package chucknorris;
+import java.util.Arrays;
+import java.util.Objects;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Input string:");
+        System.out.println("Input encoded string:");
         String input = scanner.nextLine();
+        String[] encoded = input.split(" ");
+        String output = "";
+        for (int i = 1; i < encoded.length; i++) {
+            int num = encoded[i].length();
+            if (Objects.equals(encoded[i - 1], "0")) {
+                output = output.concat("1".repeat(num));
+            }
+            if (Objects.equals(encoded[i - 1], "00")) {
+                output = output.concat("0".repeat(num));
+            }
+            i++;
+        }
+        String [] decoded = output.split("(?<=\\G.{7})");
+        System.out.println("The result:");
+        for (int i = 0; i < decoded.length; i++) {
+            int a = Integer.parseInt(decoded[i],2);
+            char c = (char)a;
+            System.out.print(c);
+        }
+
+        /*
         String lettersInBinary = "";
         char[] letters = new char[input.length()];
 
@@ -30,5 +53,6 @@ public class Main {
                 System.out.print("00 " + "0".repeat(count) + " ");
             }
         }
+        */
     }
 }
